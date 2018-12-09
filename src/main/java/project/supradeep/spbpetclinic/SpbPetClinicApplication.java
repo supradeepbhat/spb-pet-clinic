aec6dc9c37b0dca855f65b53bde3bb402e602042
supradeepbhat <sbharadwaj@hawk.iit.edu>
2018-12-09 10:21:14 -0600

    Initial Project Add

---
+++ src/main/java/project/supradeep/spbpetclinic/SpbPetClinicApplication.java
@@ -0,0 +1,12 @@
+package project.supradeep.spbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/project/supradeep/spbpetclinic/SpbPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package project.supradeep.spbpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpbPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
