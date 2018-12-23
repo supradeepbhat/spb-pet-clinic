b0dff85d39e4a92409340936a5867584b2c7ea53
supradeepbhat <sbharadwaj@hawk.iit.edu>
2018-12-23 17:32:09 -0600

    Refactored to a common CRUD repository. Closes #11

--- pet-clinic-data/src/main/java/project/supradeep/spbpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/project/supradeep/spbpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package project.supradeep.spbpetclinic.services;
 
 import project.supradeep.spbpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/project/supradeep/spbpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/project/supradeep/spbpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package project.supradeep.spbpetclinic.services;
 
 import project.supradeep.spbpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
