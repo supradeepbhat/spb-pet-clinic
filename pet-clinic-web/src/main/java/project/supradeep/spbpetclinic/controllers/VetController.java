534a38a4c9f5479c6d98e300f2b4ecd6209d6a91
supradeepbhat <sbharadwaj@hawk.iit.edu>
2018-12-24 13:54:01 -0600

    Listed the vets on index page. Closes #15.

--- pet-clinic-web/src/main/java/project/supradeep/spbpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/project/supradeep/spbpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package project.supradeep.spbpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import project.supradeep.spbpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
