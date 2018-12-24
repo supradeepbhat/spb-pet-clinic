ab5204f3c6743397c83d6836a8f91e4c75fd1c66
supradeepbhat <sbharadwaj@hawk.iit.edu>
2018-12-24 13:36:17 -0600

    Listed the owners on index page. Closes #16.

--- pet-clinic-web/src/main/java/project/supradeep/spbpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/project/supradeep/spbpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package project.supradeep.spbpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import project.supradeep.spbpetclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "/owners/index";
     }
 }
