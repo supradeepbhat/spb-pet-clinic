cfd9d6e7566cf022e0685c5dadcbef8c93c66e98
supradeepbhat <sbharadwaj@hawk.iit.edu>
2018-12-24 17:25:22 -0600

    Updated the map impl to manage settings of ID property. Closes #23.

--- pet-clinic-web/src/main/java/project/supradeep/spbpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/project/supradeep/spbpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Jack");
         owner1.setLastName("Sparrow");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Taylor");
         owner2.setLastName("Durden");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners loaded.....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Kent");
         vet1.setLastName("Clark");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Bruce");
         vet2.setLastName("Wayne");
 
