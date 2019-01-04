8d3363cf326b898b179ab656e5f85de31229600c
supradeepbhat <sbharadwaj@hawk.iit.edu>
2019-01-04 11:39:39 -0600

    Created Speciality Map Service Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/project/supradeep/spbpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package project.supradeep.spbpetclinic.services;
+
+import project.supradeep.spbpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
