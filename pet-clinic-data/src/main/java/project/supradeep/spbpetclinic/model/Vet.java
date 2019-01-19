6fb84a122b37076bc2be541bdd65cb926bf6b038
supradeepbhat <sbharadwaj@hawk.iit.edu>
2019-01-19 10:44:26 -0600

    Added project lombok and refactored the code. Closes #51

--- pet-clinic-data/src/main/java/project/supradeep/spbpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/project/supradeep/spbpetclinic/model/Vet.java
@@ -1,23 +1,28 @@
 package project.supradeep.spbpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
+
+    @Builder
+    public Vet(Long id, String firstName, String lastName, Set<Speciality> specialities) {
+        super(id, firstName, lastName);
+        this.specialities = specialities;
+    }
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name="vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
     inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
